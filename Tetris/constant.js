0c094f3bcc4471acfb9ebe3b0d69e2dc55ec081d
Preter Nils <nils.preter@bbcag.ch>
2022-04-05 09:13:35 +0200

    added constant class with some constants

---
+++ Tetris/constant.js
@@ -0,0 +1,46 @@
+const pics = [
+    'url(images/orange.jpg)',
+    'url(images/pink.jpg)',
+    'url(images/purple.jpg)',
+    'url(images/lightblue.jpg)',
+    'url(images/darkgreen.jpg)',
+    'url(images/green.jpg)',
+    'url(images/yellow.jpg)'
+]
+
+const lBlock = [
+    [1, width + 1, width * 2 + 1, 2],
+    [width, width + 1, width + 2, width * 2 + 2],
+    [1, width + 1, width * 2 + 1, width * 2],
+    [width, width * 2, width * 2 + 1, width * 2 + 2]
+]
+
+const zBlock = [
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1],
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1]
+]
+
+const tBlock = [
+    [1, width, width + 1, width + 2],
+    [1, width + 1, width + 2, width * 2 + 1],
+    [width, width + 1, width + 2, width * 2 + 1],
+    [1, width, width + 1, width * 2 + 1]
+]
+
+const oBlock = [
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1]
+]
+
+const iBlock = [
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3],
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3]
+]
+
+const theBlocks = [lBlock, zBlock, tBlock, oBlock, iBlock]
