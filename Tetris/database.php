eab248660d4de7b826d0b243e43f67855b179950
Preter Nils <nils.preter@bbcag.ch>
2022-05-03 11:06:25 +0200

    updated Code

--- Tetris/database.php
+++ Tetris/database.php
@@ -2,16 +2,17 @@
 
 include_once 'database.inc';
 $name = $_POST['name'];
+$number = $_POST['score'];
 
 
-print_r($_POST);
+// print_r($_POST);
 
 
 
 $SQL = "INSERT INTO player (playerName)VALUES ('$name')";
-
+$SQL = "SELECT DISTINCT * from player";
 $conn->query($SQL);
-echo $SQL;
+// echo $SQL;
 
 
 /*** Anzeigen 
